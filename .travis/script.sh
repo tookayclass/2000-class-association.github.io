7433b29ffa723d666a8b33e247154fd30b9ec857
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-25 13:07:13 +0100

    Add pushing

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,7 +7,8 @@ git config --global committer.name "Deployment Bot"
 git config --global author.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global author.name "${GITHUB_ACTOR}"
 
-git commit --allow-empty --message "Empty commit to trigger new Jekyll build on GitHub"
+git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
 credentials=$(echo -n "${GITHUB_ACTOR}:${GITHUB_TOKEN}" | base64)
 authheader="Authorization: Basic ${credentials}"
+git -c http.extraheader="${authheader}" push --force origin master:gh-pages
