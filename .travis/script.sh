4c988582e4021ff3bd9301d75b4d0a3752a80d69
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-25 13:20:01 +0100

    checkout branch first

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,6 +7,7 @@ git config --global committer.name "Deployment Bot"
 git config --global author.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global author.name "${GITHUB_ACTOR}"
 
+git checkout master
 git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
 credentials=$(echo -n "${GITHUB_ACTOR}:${GITHUB_TOKEN}" | base64)
