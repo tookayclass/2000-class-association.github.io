0e4d5a6b723536a3649eeb173eec8045ad81a2be
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-25 13:43:20 +0100

    Try removing git config user info

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-GITHUB_ACTOR="faph"
-
-git config committer.email "deploy@travis-ci.org"
-git config committer.name "Deployment Bot"
-git config author.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git config author.name "${GITHUB_ACTOR}"
-
 git checkout master
 git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
-credentials=$(echo -n "${GITHUB_ACTOR}:${GITHUB_TOKEN}" | base64)
+credentials=$(echo -n "travis:${GITHUB_TOKEN}" | base64)
 authheader="Authorization: Basic ${credentials}"
 git -c http.extraheader="${authheader}" push origin master:master
