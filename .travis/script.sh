62aa8ffe663709c835932ca80bc320808403459e
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-25 13:02:38 +0100

    Configure git and do empty commit (WIP)

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Hello World!"
+GITHUB_ACTOR="faph"
+
+git config --global committer.email "deploy@travis-ci.org"
+git config --global committer.name "Deployment Bot"
+git config --global author.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global author.name "${GITHUB_ACTOR}"
+
+git commit --allow-empty --message "Empty commit to trigger new Jekyll build on GitHub"
+
+credentials=$(echo -n "${GITHUB_ACTOR}:${GITHUB_TOKEN}" | base64)
+authheader="Authorization: Basic ${credentials}"
