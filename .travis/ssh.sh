44cce81bcbd980b0cac47fcd2303f52d996f497a
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-30 13:26:47 +0100

    Print config before and after (WIP)

--- .travis/ssh.sh
+++ .travis/ssh.sh
@@ -2,14 +2,16 @@
 
 add_key() {
   local key=$1;
-  local ssh_dir="${HOME}/.ssh"
+  local ssh_dir="${HOME}/.ssh";
   local destination="${ssh_dir}/pk_${TRAVIS_JOB_ID}.pem";
   local config="${ssh_dir}/config";
 
   mkdir --parents --verbose "${ssh_dir}";
 
   echo $key > $destination;
-
-  (echo "Host github.com"; echo "    IdentityFile ${destination}") > $config;
   chmod --recursive u=r,go= $destination;
+
+  cat $config;
+  (echo "Host github.com"; echo "    IdentityFile ${destination}") > $config;
+  cat $config;
 }
