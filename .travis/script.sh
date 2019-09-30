c1341fea17adb7f885cfe77231273a4f5df0556f
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-30 20:51:31 +0100

    quote repo URL

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,4 +8,4 @@ git checkout master
 git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
 repo="ssh://git@github.com:${TRAVIS_REPO_SLUG}.git"
-git push $repo master:master
+git push "${repo}" master:master
