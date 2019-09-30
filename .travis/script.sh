a07cfc34d1724b16b3743ea7cc76f7df03d94663
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-30 20:59:31 +0100

    remove ssh:// from repo URL

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,7 +7,5 @@ git config core.sshCommand "ssh -i ./.travis/id_rsa.pem"
 git checkout master
 git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
-repo="ssh://git@github.com:${TRAVIS_REPO_SLUG}.git"
-repo="git@github.com:2000-class-association/2000-class-association.github.io.git"
+repo="git@github.com:${TRAVIS_REPO_SLUG}.git"
 git push "${repo}" master:master
-
