ecf8d0cf239da22824ccca996bd2bee681219b95
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2019-09-30 20:55:56 +0100

    try this

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,4 +8,6 @@ git checkout master
 git commit --allow-empty --verbose --message "Empty commit to trigger new Jekyll build on GitHub"
 
 repo="ssh://git@github.com:${TRAVIS_REPO_SLUG}.git"
+repo="git@github.com:2000-class-association/2000-class-association.github.io.git"
 git push "${repo}" master:master
+
